7786497a38ad18277dfa736933080d78046216c5
mOHITH998 <mohithg98@gmail.com>
2021-12-01 19:04:14 +0530

    admin middleware updated

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded._id);
+    next();
+  } else {
+    res.status(401).json("Not authorized");
+  }
+  if (!token) {
+    res.status(401).json('Unauthirized')
+  }
+
+});
+
+exports.admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json(`you don't have permission`)
+  }
+};
+  // if (req.user && req.user.isAdmin) {
+  //   console.log('Welcome Admin!')
+  //   next();
+  // } else {
+  //   res.status(401).json("Not Authorized, Only admin can perform");
+  // }
+  // next()
+
